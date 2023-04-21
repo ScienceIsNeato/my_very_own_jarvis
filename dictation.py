eefd890d712fe535903bc53f308830215b0c1e0f
Will Martin <quarkswithforks@gmail.com>
2023-04-21 01:44:30 -0700

    WIP ignoring no change

--- dictation.py
+++ dictation.py
@@ -4,9 +4,21 @@ import base64
 import json
 import time
 import speech_recognition as sr
+import logging
 
 FRAMES_PER_BUFFER = 3200
 RATE = 16000
+SILENCE_THRESHOLD = 0.75  # seconds
+
+# globals
+last_call_time = time.time()
+last_line = ""
+speech_started = False
+duration_since_last_detected_change = 0.0
+silence_detected = False
+audio_detected = False
+initialized = False
+
 listen_dur_secs = 400
 device_index = None
 
@@ -15,9 +27,70 @@ source = sr.Microphone(device_index=device_index)
 
 URL = "wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000"
 
+logging.basicConfig(level=logging.INFO)
+
 def callback(recognizer, audio):
     data = audio.get_raw_data(convert_rate=RATE, convert_width=2)
     return base64.b64encode(data).decode("utf-8")
+
+
+def done_speaking(current_line):
+    global last_call_time
+    global last_line
+    global speech_started
+    global duration_since_last_detected_change
+    global silence_detected
+    global audio_detected
+    global initialized
+
+
+    try:
+        # Record the current time
+        current_time = time.time()
+
+        if not initialized:
+            last_line = current_line
+            last_call_time = current_time
+            initialized = True
+
+        # If this is the first call with actual audio, set speech_started to true and last_call_time to the current time
+        if not speech_started and current_line:
+            speech_started = True
+            last_line = current_line
+            last_call_time = time.time()
+            logging.info("Initialized started speech detection...")
+
+        if not initialized or not speech_started:
+            # Either this is the first call or we haven't detected any audio yet
+            return False
+
+        # Determine if the current input is identical to the previous input
+        if current_line == last_line:
+            # Increment duration_since_last_detected_change by the difference between the last call and this call
+            duration_since_last_detected_change += current_time - last_call_time
+            last_call_time = current_time
+        else:
+            # Reset duration_since_last_detected_change to 0
+            duration_since_last_detected_change = 0
+
+        # Determine if silence has been detected
+        if duration_since_last_detected_change >= SILENCE_THRESHOLD:
+            silence_detected = True
+        else:
+            silence_detected = False
+
+        # Update the last_call_time and last_line global variables
+        last_call_time = current_time
+        last_line = current_line
+
+        if audio_detected:
+            logging.info(f"Time elapsed since last change: {current_time - last_call_time:.2f}")
+
+        return silence_detected
+
+    except Exception as e:
+        logging.error(f"done_speaking error: {e}")
+        return False
 
 async def send_receive():
     print(f'Connecting websocket to url {URL}')
@@ -61,16 +134,25 @@ async def send_receive():
             return True
 
         async def receive():
+            is_done = False
             while True:
                 try:
                     result_str = await _ws.recv()
-                    print(json.loads(result_str)['text'])
+                    if result_str:
+                        current_phrase = json.loads(result_str)['text']
+                        if current_phrase:
+                            print(current_phrase)
+                            # Send the current phrase to the done_speaking method
+                            is_done = done_speaking(current_phrase)
+
                 except websockets.exceptions.ConnectionClosedError as e:
                     print(e)
                     assert e.code == 4008
                     break
                 except Exception as e:
                     assert False, "Not a websocket 4008 error"
+
+            return is_done
 
         send_result, receive_result = await asyncio.gather(send(), receive())
 
