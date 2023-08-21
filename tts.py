c3c518bbe757fbe971af16ac2d9466241410e838
Will Martin <quarkswithforks@gmail.com>
2023-08-20 23:44:16 -0700

    finishing getting coqui tts up and running in here

--- tts.py
+++ tts.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
-import json
 from gtts import gTTS
 from pydub import AudioSegment
 from pydub.playback import play
 from datetime import datetime
+import subprocess
+import os
 
 import requests
 
@@ -62,33 +63,62 @@ class CoquiTTS(TextToSpeech):
         self.bearer_token = bearer_token
         self.voice_id = voice_id
 
+    def split_text(self, text: str, max_length: int = 250):
+        words = text.split()
+        chunks = []
+        current_chunk = ""
+        for word in words:
+            if len(current_chunk) + len(word) + 1 > max_length:
+                chunks.append(current_chunk)
+                current_chunk = ""
+            current_chunk += (" " if current_chunk else "") + word
+        if current_chunk:
+            chunks.append(current_chunk)
+        return chunks
+
     def convert_text_to_speech(self, text: str):
         try:
-            payload = {
-                "name": "my test sample",
-                "voice_id": self.voice_id,
-                "text": text
-            }
+            chunks = self.split_text(text)
+            files = []
+            for chunk in chunks:
+                payload = {
+                    "name": "GANGLIA",
+                    "voice_id": self.voice_id,
+                    "text": chunk
+                }
 
-            headers = {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer " + self.bearer_token,
-                "Accept": "application/json"
-            }
+                headers = {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + self.bearer_token,
+                    "Accept": "application/json"
+                }
 
-            response = requests.post(self.api_url, data=json.dumps(payload), headers=headers)
-            audio_url = response.json()["audio_url"]
-            
-            if not audio_url:
-                print("No audio url found in the response")
-                return 1, None
-            
-            file_path = f"/tmp/chatgpt_response_{datetime.now().strftime('%Y%m%d-%H%M%S')}.mp3"
-            audio_response = requests.get(audio_url)
-            with open(file_path, 'wb') as audio_file:
-                audio_file.write(audio_response.content)
-            
-            return 0, file_path
+                response = requests.post(self.api_url, json=payload, headers=headers)
+                audio_url = response.json().get("audio_url")
+
+                if not audio_url:
+                    print(f"No audio url found in the response for chunk: {chunk}")
+                    continue
+
+                file_path = os.path.abspath(f"/tmp/chatgpt_response_{datetime.now().strftime('%Y%m%d-%H%M%S')}.mp3")
+                audio_response = requests.get(audio_url)
+                with open(file_path, 'wb') as audio_file:
+                    audio_file.write(audio_response.content)
+                files.append(file_path)
+
+            # Write the list of files to a temporary file
+            list_file_path = "/tmp/concat_list.txt"
+            with open(list_file_path, 'w') as list_file:
+                list_file.write('\n'.join(f"file '{file}'" for file in files))
+
+            print(f"Written file list to {list_file_path}")  # Logging
+
+            # Use FFmpeg to concatenate the files together
+            output_file = "combined_audio.mp3"
+            command = f"ffmpeg -y -f concat -safe 0 -i {list_file_path} {output_file}"
+            subprocess.run(command, shell=True)
+
+            return 0, output_file
         except Exception as e:
             print(f"Error converting text to speech: {e}")
             return 1, None
