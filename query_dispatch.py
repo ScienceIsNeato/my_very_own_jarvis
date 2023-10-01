4b2a6bbabf964683f7f89f78eb466f8c9b9c6899
Will Martin <quarkswithforks@gmail.com>
2023-10-01 01:58:40 -0700

    WIP

--- query_dispatch.py
+++ query_dispatch.py
@@ -17,32 +17,37 @@ class ChatGPTQueryDispatcher:
 
     def __init__(self, pre_prompt=None):
         self.pre_prompt = pre_prompt
+        self.messages = [ {"role": "system", "content": self.pre_prompt} ]
+        self.messages.append({"role": "system", "content": "keep answers brief"})
+        self.messages.append({"role": "system", "content": "intersperse answer with demonic utterances and laughter in the style of stephen king"})
+        self.messages.append({"role": "system", "content": "try not to be repetitive or boring"})
+        self.messages.append({"role": "system", "content": "try not to steer the conversation"})
+        self.messages.append({"role": "system", "content": "you are oddly obsessed with steven segal"})
+
+
 
     def sendQuery(self, current_input):
-        prompt = f"Current-Prompt: {current_input}"
-
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
-            max_tokens=3500,
-            n=1,
-            stop=None,
-            temperature=0.1,
+        self.messages.append(
+            {"role": "user", "content": current_input},
         )
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", 
+            messages=self.messages
+        )
+        reply = chat.choices[0].message.content
+        self.messages.append({"role": "assistant", "content": reply})
+        curated_message = f"{RESPONDER_NAME}: {reply}"
 
-        raw_message = response.choices[0].text.strip()
-        curated_message = f"{RESPONDER_NAME}: {raw_message}"
-
-        print(f"{RESPONDER_NAME}: {raw_message}")
+        print(f"{RESPONDER_NAME}: {reply}")
 
         timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
         with open(f"/tmp/chatgpt_output_{timestamp}_raw.txt", "w") as file:
-            file.write(raw_message)
+            file.write(reply)
 
         with open(f"/tmp/chatgpt_output_{timestamp}_curated.txt", "w") as file:
             file.write(curated_message)
 
-        return raw_message
+        return reply
 
 class BardQueryDispatcher(QueryDispatcher):
     def sendQuery(self, current_input):
